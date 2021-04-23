0133c170c8d996675b7c40cc6e4a00af01e72fa8
Malek Chtioui <chtiouimalek001@gmail.com>
2021-04-23 23:17:17 +0100

    Creation de table et model Employe

---
+++ database/migrations/2021_04_23_215951_create_employes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name',70);
+            $table->string('last_name',70);
+            $table->integer('phone_number');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employes');
+    }
+}
