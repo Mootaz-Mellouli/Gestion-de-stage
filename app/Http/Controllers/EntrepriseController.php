57eb010075e8e117b13ed43e48fc1fecd87c66e3
bilel713 <bilelbenaissa06@gmail.com>
2021-06-02 20:20:31 +0200

    entreprise-template+

---
+++ app/Http/Controllers/EntrepriseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Entreprise;
+use Illuminate\Http\Request;
+
+class EntrepriseController extends Controller
+{
+    public function entrepriseList()
+    {
+        $entreprise = Entreprise::all();
+        return view('layouts.entreprise', ['entreprise' => $entreprise]);
+    }
+}
